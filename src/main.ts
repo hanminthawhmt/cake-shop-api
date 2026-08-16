8bafdd6f1b2e1e0bc5b25aba396a7e5927332ddf
hanminthawhmt <hanminthaw15@gmail.com>
2026-08-17 00:20:38 +0700

    feat(swagger): integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Cake Shop API')
+    .setDescription('API documentation for the Cake Shop System')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
